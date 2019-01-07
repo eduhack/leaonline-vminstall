d3268a4c4c47b8ca8038ac875456b947b21b5556
Jan Küster <jkuester@uni-bremen.de>
2019-01-07 14:44:43 +0100

    Create install_docker.sh

---
+++ install_docker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# this is a slightly modified version
+# of the docker convenience script
+
+apt-get update
+apt-get install \
+    git \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install docker-ce docker-compose
+
+
